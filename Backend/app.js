41b6330661d72e4fd588169146ce1204466c54d3
Aditya Navrange <adityanavrange13@gmail.com>
2025-01-28 02:42:51 +0530

    Add CORS middleware to enable cross-origin requests

--- Backend/app.js
+++ Backend/app.js
@@ -3,18 +3,28 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
+
+
 
 const userRoute = require("./src/routes/user.route");
 const adminRoute = require("./src/routes/admin.route");
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message:"Testing api!!!!"
-    })
-})
+  res.status(200).json({
+    message: "Testing api!!!!",
+  });
+});
 
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/admin", adminRoute);
