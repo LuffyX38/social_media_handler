f2db7cc0b4a2e459ba7a6073e940859327b01362
Aditya Navrange <adityanavrange13@gmail.com>
2025-01-28 02:43:20 +0530

    Remove unused route for showing users in user.route.js

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -3,7 +3,6 @@ const userController = require("../controllers/user.controller");
 const upload = require("../middleware/multer");
 const auth = require("../middleware/auth");
 
-router.route("/").get(userController.showUsers);
 router.route("/create-user").post(upload.array("images"),userController.createUser);
 router.route("/show-users").get(auth.verifyJWT,userController.showUsers);
 
